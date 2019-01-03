6e7a234c826d038b6ba62c2d10c75005a9eb6db3
Sal Calabrese <erotavlas103@gmail.com>
2019-01-03 10:16:58 -0500

    add user input for loop

--- Chapter 1/chaos.py
+++ Chapter 1/chaos.py
@@ -12,8 +12,16 @@
 #    print(x)
 
 #Modify program to print 20 lines instead of 10
+#print("This program illstratea a chaotic function")
+#x = eval(input("Enter a number between 0 and 1: "))
+#for i in range(20):
+#    x = 2.0 * x * (1 - x)
+#    print(x)
+
+#Modify program to get the number of repetitions from the user
 print("This program illstratea a chaotic function")
 x = eval(input("Enter a number between 0 and 1: "))
-for i in range(20):
+n = eval(input("Enter the number of times I should repeat: "))
+for i in range(n):
     x = 2.0 * x * (1 - x)
-    print(x)
+    print(x)    
