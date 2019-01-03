4cb0adcdc956032d0085e59e69f1fd8c12890ffe
Sal Calabrese <erotavlas103@gmail.com>
2019-01-03 14:33:18 -0500

    add columns for output

--- Chapter 1/chaos.py
+++ Chapter 1/chaos.py
@@ -26,11 +26,23 @@
 #    x = 2.0 * x * (1 - x)
 #    print(x)
     
-#Run the algebraic equation 3 equivalent ways and compare them
-print("This program illstratea a chaotic function")
-x = eval(input("Enter a number between 0 and 1: "))
+#Run the algebraic equation 3 equivalent ways and compare them 100 times
+#print("This program illstratea a chaotic function")
+#x = eval(input("Enter a number between 0 and 1: "))
+#for i in range(100):
+#    x = 3.9 * x * (1 - x)
+#    y = 3.9 * (x - x * x)
+#    z = 3.9 * x - 3.9 * x * x
+#    print(x, y, z)
+
+#Run the basic chaos program and print into columns for two values
+
+print("input     0.25            0.26  ")
+print("_"*32)
+x = 0.25
+y = 0.26
+
 for i in range(10):
     x = 3.9 * x * (1 - x)
-    y = 3.9 * (x - x * x)
-    z = 3.9 * x - 3.9 * x * x
-    print(x, y, z)
+    y = 3.9 * y * (1 - y)
+    print("       {}             {}".format(round(x, 6), round(y, 6)))
