faa1c7098aae2003295d5700018f5f30b1d6a11f
Sal Calabrese <erotavlas103@gmail.com>
2019-01-03 10:06:13 -0500

    modify expression

--- Chapter 1/chaos.py
+++ Chapter 1/chaos.py
@@ -1,5 +1,5 @@
 print("This program illstratea a chaotic function")
 x = eval(input("Enter a number between 0 and 1: "))
 for i in range(10):
-    x = 3.9 * x * (1 - x)
+    x = 2.0 * x * (1 - x)
     print(x)
