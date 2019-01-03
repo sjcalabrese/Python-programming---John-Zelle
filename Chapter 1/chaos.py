022436ca9d1419a905902dcd13f1927e87305b34
Sal Calabrese <erotavlas103@gmail.com>
2019-01-03 10:12:19 -0500

    Update loop count

--- Chapter 1/chaos.py
+++ Chapter 1/chaos.py
@@ -1,5 +1,19 @@
+#print("This program illstratea a chaotic function")
+#x = eval(input("Enter a number between 0 and 1: "))
+#for i in range(10):
+#    x = 3.9 * x * (1 - x)
+#    print(x)
+
+#Modify the expression (removed 3.9 and added 2.0)
+#print("This program illstratea a chaotic function")
+#x = eval(input("Enter a number between 0 and 1: "))
+#for i in range(10):
+#    x = 2.0 * x * (1 - x)
+#    print(x)
+
+#Modify program to print 20 lines instead of 10
 print("This program illstratea a chaotic function")
 x = eval(input("Enter a number between 0 and 1: "))
-for i in range(10):
+for i in range(20):
     x = 2.0 * x * (1 - x)
     print(x)
