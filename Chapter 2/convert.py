942ee49f2f5ff128866001b2eb044b8dec79dfd7
Sal Calabrese <erotavlas103@gmail.com>
2019-01-04 12:24:03 -0500

    add user input to quit program

--- Chapter 2/convert.py
+++ Chapter 2/convert.py
@@ -3,5 +3,6 @@ def main():
     celsius = eval(input("What is the Cellsius temperature? "))
     fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    input("Press the <Enter> key to quit.")
 
 main()
