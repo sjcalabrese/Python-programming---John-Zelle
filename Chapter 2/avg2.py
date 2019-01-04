bb1d7b280dbc9c45d71a4a6fb8571fee407a4eec
Sal Calabrese <erotavlas103@gmail.com>
2019-01-04 14:25:46 -0500

    initial commit

---
+++ Chapter 2/avg2.py
@@ -0,0 +1,12 @@
+#A simple program to average two exam scores
+#Illustrates use of multiple input
+
+def main():
+    print("This program computes the averae of two exam scores.")
+    
+    score1, score2 = eval(input("Enter two scores separated by a comma: "))
+    average = (score1 + score2) / 2
+    
+    print("The average of the score is:", average)
+    
+main()
