35e6cfb174eaf06c320bfed13317081c13ed07ae
Sal Calabrese <erotavlas103@gmail.com>
2019-01-04 12:22:40 -0500

    add intro

--- Chapter 2/convert.py
+++ Chapter 2/convert.py
@@ -1,4 +1,5 @@
 def main():
+    print("This is a program to conver the temperature from Celsius to Fehrenheit")
     celsius = eval(input("What is the Cellsius temperature? "))
     fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
