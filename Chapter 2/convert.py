3346ecc34a5bcbf189eaa2a263c0f3307bdd0673
Sal Calabrese <erotavlas103@gmail.com>
2019-01-07 10:09:59 -0500

    add ability to repeat 5 times

--- Chapter 2/convert.py
+++ Chapter 2/convert.py
@@ -1,8 +1,9 @@
 def main():
     print("This is a program to conver the temperature from Celsius to Fehrenheit")
-    celsius = eval(input("What is the Cellsius temperature? "))
-    fahrenheit = 9/5 * celsius + 32
-    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+    for i in range(5):
+        celsius = eval(input("What is the Cellsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
     input("Press the <Enter> key to quit.")
 
 main()
