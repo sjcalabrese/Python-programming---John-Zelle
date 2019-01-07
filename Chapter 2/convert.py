8d0f05bfb08c01e80da452ca8f9677f9d37c7731
Sal Calabrese <erotavlas103@gmail.com>
2019-01-07 10:14:10 -0500

    Modify to calculate every 10 degrees

--- Chapter 2/convert.py
+++ Chapter 2/convert.py
@@ -1,8 +1,8 @@
 def main():
     print("This is a program to conver the temperature from Celsius to Fehrenheit")
-    for i in range(5):
-        celsius = eval(input("What is the Cellsius temperature? "))
-        fahrenheit = 9/5 * celsius + 32
+    for i in range(0,110,10):
+        celsius = i
+        fahrenheit = 9/5 * i + 32
         print("The temperature is", fahrenheit, "degrees Fahrenheit.")
     input("Press the <Enter> key to quit.")
 
