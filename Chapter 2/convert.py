f048afb31d01e9cce47f646dc76e0e9d9c2e6f50
Sal Calabrese <erotavlas103@gmail.com>
2019-01-07 12:10:51 -0500

    update output to show a table

--- Chapter 2/convert.py
+++ Chapter 2/convert.py
@@ -1,9 +1,12 @@
 def main():
     print("This is a program to conver the temperature from Celsius to Fehrenheit")
+    print()
+    print("celsius   fahrenheit")
+    print("____________________")    
     for i in range(0,110,10):
         celsius = i
         fahrenheit = 9/5 * i + 32
-        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+        print("{}            {}".format(i, fahrenheit))
     input("Press the <Enter> key to quit.")
 
 main()
